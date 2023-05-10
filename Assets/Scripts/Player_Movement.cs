b9a9cffafa9f7ce94367994b79810fbeb25ace7b
Kacper Wojdyła <kacper.w.wojdyla@gmail.com>
2023-05-10 19:22:08 +0200

    add everything

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
